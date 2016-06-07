28d7f6a0e7436c281f1ba1a4af511c0ad8d14b3d
takamichi <takamichi@tanaka-suzuka-no-MacBook.local>
2016-06-07 11:50:01 +0900

    test

---
+++ testmanagement/WEB-INF/src/jp/asojuku/testmanagement/entity/MemberEntity.java
@@ -0,0 +1,17 @@
+package jp.asojuku.testmanagement.entity;
+
+public class MemberEntity {
+	
+	public static String MAIL_ADRESS="MAIL_ADRESS";
+	public static String PASSWORD="PASSWORD";
+	private String mailAddress;
+	
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	
+
+}
